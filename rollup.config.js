2aa103d791f2e5304d71199591468dc790360608
dhershman1 <dustinh17@gmail.com>
2018-06-29 14:29:33 -0400

    compress build down to a single config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,38 @@ import babel from 'rollup-plugin-babel'
 import cleanup from 'rollup-plugin-cleanup'
 import filesize from 'rollup-plugin-filesize'
 import { uglify } from 'rollup-plugin-uglify'
+import globby from 'globby'
+import path from 'path'
+
+const buildEntry = () => {
+  const results = []
+  const paths = globby.sync(['src/*.js', '!src/main.js', '!src/_internals'])
+
+  paths.forEach(p => {
+    const { name, base } = path.parse(p)
+
+    const config = {
+      input: p,
+      plugins: [
+        babel(),
+        uglify(),
+        filesize()
+      ],
+      output: {
+        file: name === 'index' ? 'esm.js' : base,
+        format: 'umd',
+        name: name === 'index' ? 'esm' : name,
+        exports: 'named'
+      }
+    }
+
+    results.push(config)
+
+    return true
+  })
+
+  return results
+}
 
 export default [{
   input: './src/main.js',
@@ -29,4 +61,4 @@ export default [{
     name: 'simplyValid',
     exports: 'default'
   }
-}]
+}].concat(buildEntry())
