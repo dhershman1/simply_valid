f8342a6a431fa5ad5fc45f1b86fbae64991f1f62
dhershman1 <dustinh17@gmail.com>
2018-06-29 14:24:59 -0400

    Imrpoved clean script renamed config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import cleanup from 'rollup-plugin-cleanup'
 import filesize from 'rollup-plugin-filesize'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default [{
   input: './src/main.js',
