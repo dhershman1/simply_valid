d7926719be198ca2c1c22b5210ef553f0acb2531
dhershman1 <dustinh17@gmail.com>
2018-11-27 17:02:11 -0500

    Tweaking main js some more

--- src/main.js
+++ src/main.js
@@ -1,9 +1,82 @@
-import { curry, type } from 'kyanite'
+import { always, branch, curry, ensureArray, eq, type } from 'kyanite'
 
-const validateSchema = schema =>
-  (Array.isArray(schema) && schema.length) ||
-  (type(schema) === 'Object' && Object.keys(schema).length) ||
-  Boolean(schema.length)
+/**
+ * @private
+ * @function
+ * Runs an array of validation functions against given data
+ * @param {Array} methods An Array of validation functions
+ * @param {Array} data The data to validate
+ */
+const arrValidate = curry((methods, data) => {
+  if (!Array.isArray(methods)) {
+    return data.every(methods)
+  }
+
+  for (let i = 0, len = methods.length; i < len; i++) {
+    const fn = methods[i]
+
+    if (!ensureArray(data).every(fn)) {
+      return {
+        isValid: false,
+        rule: fn.name,
+        data
+      }
+    }
+  }
+
+  return {
+    isValid: true
+  }
+})
+
+/**
+ * @function
+ * @private
+ * Runs a validation schema against a provided object of data
+ * If the schema is an object the data provided must also be an object
+ * @param {Object} schema The object schema of validation methods
+ * @param {Object} data The data object to validate
+ */
+const objValidate = (schema, data) => {
+  if (type(data) !== 'Object') {
+    throw new TypeError('Data must be an object if the provided schema is an object')
+  }
+
+  const keys = Object.keys(schema)
+
+  for (let i = 0, len = keys.length; i < len; i++) {
+    const k = keys[i]
+    const fn = schema[k]
+    const value = data[k]
+
+    // Run the proper function to handle these situations
+    const valid = branch(
+      always(Array.isArray(fn)),
+      arrValidate(fn),
+      fn,
+      value
+    )
+
+    // A nested validate call failed, so back out and show that return
+    if (eq(valid.isValid, false)) {
+      return valid
+    }
+
+    // Check to see if the function passed or not
+    if (!valid) {
+      return {
+        isValid: false,
+        prop: k,
+        rule: fn.name,
+        data: value
+      }
+    }
+  }
+
+  return {
+    isValid: true
+  }
+}
 
 /**
  * @name validate
@@ -11,31 +84,52 @@ const validateSchema = schema =>
  * @function
  * @category Main
  * @description The main validation functionality of simply valid
- * @param {Object} options The main options to setup simply_valid
- * @property {Any} schema The schema that the functionality of the module should be following
- * @property {Boolean} partial Determines whether objects with missing values should fail or not
+ * @param {Object|Array} schema The schema that the functionality of the module should be following
  * @param {Any} data The data that we want to run the validation functionality against
  * @returns {Object} Returns an object with a isValid prop telling if validation was a success, and a story which is an array of objects of which validation methods failed
  *
  * @example
- * const placeholder = true
+ * import { validate, isNumber, hasNumber, hasLetters, noNumbers } from 'simply_valid'
+ *
+ * // Validating Objects
+ * validate({ zip: isNumber, address: [hasNumbers, hasLetters] }, { zip: 12345, address: '123 Test St' })
+ * // => { isValid: true }
+ *
+ * // Validating Nested Objects (Call validate again on nested objects)
+ * validate({
+ *   zip: isNumber,
+ *   address: validate({
+ *     num: isNumber,
+ *     street: [noNumbers, hasLetters]
+ *   })
+ * }, {
+ *   zip: 12345,
+ *   address: {
+ *     num: 123,
+ *     stree: 'Test St'
+ *   }
+ * })
+ * // => { isValid: true }
+ *
+ * // Validating Arrays
+ * validate([isNumber, isPositive], [1, 2, 3, 4, 5])
+ * // => { isValid: true }
+ *
+ * // This is also all curried
+ * const fn = validate({ zip: isNumber, address: [hasLetters, hasNumbers] })
+ *
+ * fn({ zip: 12345, address: '123 Test St' }) // => { isValid: true }
  */
-const validate = (options, data) => {
-  const defaults = {
-    schema: [],
-    partial: true
+const validate = (schema, data) => {
+  if (!Array.isArray(schema) && type(schema) !== 'Object') {
+    throw new TypeError('The Schema should either be an Array or Object')
   }
-  const opts = Object.keys(options).reduce((acc, key) => {
-    if (acc[key]) {
-      acc[key] = options[key]
-    }
 
-    return acc
-  }, defaults)
-
-  if (!validateSchema(opts.schema)) {
-    throw new Error('The schema is either invalid or one was not provided for validation')
+  if (Array.isArray(schema)) {
+    return arrValidate(schema, data)
   }
+
+  return objValidate(schema, data)
 }
 
 export default curry(validate)
--- tests/main.js
+++ tests/main.js
@@ -1,229 +1,32 @@
-import { validate } from '../src/index'
+import { validate, isNumber, hasLetters, hasNumbers, isPositive, meetsPassReq, noLetters, meetsMinMax } from '../src/index'
 import test from 'tape'
 
 const testData = {
   zip: '11445',
-  address: '1132 Cool St'
+  address: '1132 Cool St',
+  pass: 'turd',
+  between: 20
 }
 
-test('Test String Schema', t => {
-  const valid = validate({
-    schema: 'hasValue'
-  })
+test('Rando', t => {
+  // const isValid = validate({
+  //   zip: isNumber,
+  //   address: hasLetters
+  // })
 
-  t.ok(valid)
-  t.ok(valid('cool').isValid, 'Validation passed')
-  t.notOk(valid('').isValid, 'Empty string is not valid')
-  t.end()
-})
+  testData.nested = { inner: '10' }
 
-test('Test Object Schema', t => {
-  const valid = validate({
-    schema: {
-      zip: ['isNumber'],
-      address: ['hasLetters', 'hasNumbers']
-    }
-  })
+  // console.log(isValid(testData))
+  // console.log(isValid({ zip: 44114, address: 1123 }))
+  console.log(validate({
+    zip: isNumber,
+    address: [hasLetters, hasNumbers],
+    between: meetsMinMax(1, 10),
+    pass: meetsPassReq,
+    nested: validate({ inner: noLetters })
+  }, testData))
+  console.log(validate([isNumber, isPositive], -1))
 
-  t.ok(valid)
-  t.ok(valid(testData).isValid, 'Object is a valid object')
-  t.notOk(valid({
-    zip: 'cool',
-    address: '112 test St'
-  }).isValid, 'Object is invalid')
-  t.end()
-})
-
-test('Test Object Schema Omitting values', t => {
-  const valid = validate({
-    schema: {
-      zip: ['hasValue', 'isNumber']
-    }
-  })
-
-  t.ok(valid)
-  t.ok(valid(testData).isValid, 'Object is a valid object')
-  t.notOk(valid({
-    zip: '',
-    address: '112 test St'
-  }).isValid, 'Object is invalid')
-  t.end()
-})
-
-test('Test Array Schema', t => {
-  const valid = validate({
-    schema: ['isNumber', 'isPositive']
-  })
-
-  t.ok(valid)
-  t.ok(valid('5').isValid, 'Number is valid and positive')
-  t.notOk(valid('-4').isValid, 'Number is negative and not valid')
-  t.end()
-})
-
-test('Test nested Object', t => {
-  const valid = validate({
-    schema: {
-      test: ['isPositive'],
-      info: {
-        zip: ['hasValue', 'hasNumbers'],
-        address: ['hasLetters']
-      }
-    }
-  })
-
-  t.ok(valid)
-  t.ok(valid({
-    test: '4',
-    info: {
-      zip: '44432',
-      address: '4432 Test St'
-    }
-  }).isValid, 'Valid nested object')
-  t.end()
-})
-
-test('Test nested object with arrays', t => {
-  const valid = validate({
-    schema: {
-      test: {
-        arraytest: ['isPositive']
-      },
-      info: {
-        zip: ['hasValue', 'hasNumbers'],
-        moreInfo: {
-          address: ['hasLetters']
-        }
-      }
-    }
-  })
-
-  const results = valid({
-    test: {
-      arraytest: ['3', '2', '1', '5']
-    },
-    info: {
-      zip: '44564',
-      moreInfo: {
-        address: '11234 Test Rd'
-      }
-    }
-  })
-
-  t.ok(valid)
-  t.ok(results.isValid, 'Is a valid object')
-  t.is(results.story.length, 0, 'There is no story')
-  t.end()
-})
-
-test('Testing Combo Functionality', t => {
-  const simple = validate({
-    schema: {
-      cc: 'creditCard',
-      date: 'date',
-      cvn: 'cvn',
-      zip: 'zipOrPostal'
-    }
-  })
-  const results = simple({
-    cc: '4012888888881881',
-    date: '01/18',
-    cvn: '333',
-    zip: '55555'
-  })
-
-  t.ok(results)
-  t.ok(results.isValid)
-  t.end()
-})
-
-test('Testing Combo Functionality Mixup', t => {
-  const simple = validate({
-    schema: {
-      cc: 'creditCard',
-      date: 'date',
-      cvn: 'cvn',
-      zip: 'zipOrPostal'
-    }
-  })
-  const results = simple({
-    cc: '4012888888881881',
-    date: '01/20/18',
-    cvn: '3333',
-    zip: 'K1A0B1'
-  })
-
-  t.ok(results)
-  t.ok(results.isValid)
-  t.end()
-})
-
-test('Test edge case for objects in schema', t => {
-  const data = {
-    email: 'dusty@gmail.com',
-    zip: '44114',
-    name: 'firstname',
-    phone: {
-      area: 'a123',
-      local: '-999'
-    }
-  }
-  const schema = {
-    email: 'isEmail',
-    zip: 'isZip',
-    name: ['hasValue', 'hasLetters'],
-    phone: {
-      area: ['noLetters', 'hasNumbers'],
-      local: 'noSpecials'
-    }
-  }
-  const results = validate({ schema }, data)
-
-  t.notOk(results.isValid)
-  t.is(results.story[0].test, 'noLetters')
-  t.is(results.story[1].test, 'noSpecials')
-  t.end()
-})
-
-test('Testing main validation for Arrays', t => {
-  const results = validate({ schema: ['hasNumbers', 'hasLetters'] }, ['henlo112', 'blep102'])
-
-  t.ok(results.isValid)
-  t.end()
-})
-
-test('Testing main validation for Strings', t => {
-  const results = validate({ schema: 'hasNumbers' }, 'blep')
-
-  t.notOk(results.isValid)
-  t.end()
-})
-
-test('Throws an error when the schema passed in is invalid', t => {
-  try {
-    validate({ schema: [] }, 'test')
-  } catch (err) {
-    t.is(err.message, 'The schema is either invalid or one was not provided for validation')
-    t.end()
-  }
-})
-
-test('Handles if a random invalid option is passed in', t => {
-  const validator = validate({ schema: ['isPositive'], hi: 'test' })
-
-  t.ok(validator(1).isValid)
-  t.end()
-})
-
-test('Handles if certain object props do not exist', t => {
-  const valid = validate({
-    schema: {
-      zip: 'isNumber',
-      address: 'hasLetters',
-      num: 'isPositive'
-    }
-  })
-
-  console.log(valid({ zip: 112345, address: '123 test st' }))
+  t.ok(true)
   t.end()
 })
