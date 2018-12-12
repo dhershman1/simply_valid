0d45ba11a336f0225563d5ba66586f53e9d4ed84
dhershman1 <dustinh17@gmail.com>
2018-12-12 14:34:10 -0500

    Fixed main tests

--- tests/main.js
+++ tests/main.js
@@ -1,49 +1,47 @@
-import { isBetween, validate, isNumber, hasLetters, hasNumbers, isPositive, meetsPassReq, noLetters } from '../src/index'
+import { isBetween, validate, isNumber, hasLetters, hasNumbers, meetsPassReq, isPositive } from '../src/index'
 import test from 'tape'
 
-const testData = {
-  zip: '11445',
-  address: '1132 Cool St',
-  pass: 'turd',
-  between: 20
-}
+test('validate -- Single data functionality', t => {
+  const v = validate([isNumber, isPositive])
 
-test('Rando', t => {
-  const isValid = validate({
+  t.same(v(1), { isValid: true })
+  t.same(v('1'), { isValid: true })
+  t.same(v(-1), { isValid: false, rule: 'isPositive', data: -1 })
+  t.same(v('-1'), { isValid: false, rule: 'isPositive', data: '-1' })
+  t.same(v('abc'), { isValid: false, rule: 'isNumber', data: 'abc' })
+  t.end()
+})
+
+test('validate -- Array based functionality', t => {
+  const v = validate([isNumber, isPositive])
+
+  t.same(v([1, 2, 3]), { isValid: true })
+  t.same(v([1, '2', 3]), { isValid: true })
+  t.same(v([1, 2, -3]), { isValid: false, rule: 'isPositive', data: [1, 2, -3] })
+  t.same(v([1, 2, '-3']), { isValid: false, rule: 'isPositive', data: [1, 2, '-3'] })
+  t.same(v([1, 2, 'abc']), { isValid: false, rule: 'isNumber', data: [1, 2, 'abc'] })
+  t.end()
+})
+
+test('validate -- Object based functionality', t => {
+  const v = validate({
     zip: isNumber,
-    address: hasNumbers
+    addres: [hasNumbers, hasLetters],
+    pass: meetsPassReq,
+    between: isBetween([0, 20])
   })
 
-  const isEven = val => val % 2 === 0
-  // Multi param functions must be partial
-  // And the value always comes last
-  const notMin = function notMin (min) {
-    return function _notMin (val) {
-      return val !== min
-    }
-  }
-
-  console.log(validate({
-    foo: isEven,
-    bar: [isEven, notMin(4)]
-  }, {
-      foo: 4,
-      bar: 5
-    }))
-
-  testData.nested = { inner: '10a' }
-
-  // console.log(isValid(testData))
-  // console.log(isValid({ zip: 44114, address: 1123 }))
-  // console.log(validate({
-  //   zip: isNumber,
-  //   address: [hasLetters, hasNumbers],
-    // between: isBetween(1, 10),
-    // pass: meetsPassReq,
-  //   nested: validate({ inner: noLetters })
-  // }, testData))
-  // console.log(validate([isNumber, isPositive], -1))
-
-  t.ok(true)
+  t.same(v({
+    zip: '11445',
+    address: '1132 Cool St',
+    pass: 'turd',
+    between: 20
+  }), { isValid: false, prop: 'pass', rule: 'meetsPassReq', data: 'turd' })
+  t.same(v({
+    zip: '11445',
+    address: '1132 Cool St',
+    pass: 'c00Lk1d$17',
+    between: 20
+  }), { isValid: true })
   t.end()
 })
